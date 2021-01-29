081bc541eb588e328473f8b80112b79e20300de2
Anthony Gonzalez <boilercoding@gmail.com>
2021-01-29 03:14:31 -0500

    adds configs and minor tweaks

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -17,10 +17,13 @@ module.exports = {
       // Build your palette here
       transparent: 'transparent',
       current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
       gray: colors.trueGray,
       red: colors.red,
       blue: colors.lightBlue,
       yellow: colors.amber,
+      pink: colors.pink,
     }
   }
 }
